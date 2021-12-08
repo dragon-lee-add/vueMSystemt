dbb7ccf29409ca47e6d978ccf0a03509b1c51b9f
liyouxiu <2547174342@qq.com>
2021-12-08 20:30:00 +0800

    update

--- vue-cli-demo/src/components/Home.vue
+++ vue-cli-demo/src/components/Home.vue
@@ -47,7 +47,31 @@
             </el-dropdown>
           </div>
         </el-header>
+
+        <el-card class="box-card" v-if="showTags">
+            <span class="tags-li" v-for="(item,index) in tagsList" :class="{'active': isActive(item.path)}" :key="index">
+              <router-link :to="item.path" class="tags-li-title">
+                {{item.name}}
+              </router-link>
+              <span class="tags-li-icon" @click="closeTags(index)"><i class="el-icon-close"></i></span>
+            </span>
+          
+          <!-- 其他操作按钮 -->
+          <div class="tags-close-box">
+            <el-dropdown @command="handleTags">
+              <el-button size="mini">
+                <i class="el-icon-arrow-down el-icon--right"></i>
+              </el-button>
+              <el-dropdown-menu size="small" slot="dropdown">
+                <el-dropdown-item command="other">关闭其他</el-dropdown-item>
+                <el-dropdown-item command="all">关闭所有</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+        </el-card>
+
         <router-view />
+
       </el-container>
     </el-container>
 
@@ -65,23 +89,28 @@
     },
     data() {
       return {
-        editableTabsValue: '2',
-        editableTabs: [{
-          title: 'Tab 1',
-          name: '1',
-          content: 'Tab 1 content'
-        }, {
-          title: 'Tab 2',
-          name: '2',
-          content: 'Tab 2 content'
-        }],
-        tabIndex: 2
+        tagsList: []
       };
     },
     computed: {
+      showTags() {
+        return this.tagsList.length > 0;
+      },
+
       Name() {
         return this.$store.getters.getName;
       }
+    },
+    watch: {
+      // 对router进行监听，每当访问router时，对tags的进行修改
+      $route(newValue) {
+        this.setTags(newValue);
+      }
+    },
+    created() {
+      // 第一次进入页面时，修改tag的值
+      this.setTags(this.$route);
+
     },
     mounted() {
       //在页面加载时读取sessionStorage里的状态信息
@@ -94,6 +123,46 @@
       })
     },
     methods: {
+      isActive(path) {
+        return path === this.$route.fullPath;
+      },
+      // 关闭单个标签
+      closeTags(index) {
+        const delItem = this.tagsList.splice(index, 1)[0];
+        const item = this.tagsList[index] ? this.tagsList[index] : this.tagsList[index - 1];
+        if (item) {
+          delItem.path === this.$route.fullPath && this.$router.push(item.path);
+        } else {
+          this.$router.push('/');
+        }
+      },
+      // 关闭全部标签
+      closeAll() {
+        this.tagsList = [];
+        this.$router.push('/');
+      },
+      // 关闭其他标签
+      closeOther() {
+        const curItem = this.tagsList.filter(item => {
+          return item.path === this.$route.fullPath;
+        })
+        this.tagsList = curItem;
+      },
+      // 设置标签
+      setTags(route) {
+        const isExist = this.tagsList.some(item => {
+          return item.path === route.fullPath;
+        })
+        !isExist && this.tagsList.push({
+          title: route.meta.title,
+          path: route.fullPath,
+          name: route.matched[1].components.default.name
+        })
+      },
+      // 当关闭所有页面时隐藏
+      handleTags(command) {
+        command === 'other' ? this.closeOther() : this.closeAll();
+      },
       setUp() {
         this.$refs.childSetUp.show();
 
@@ -157,5 +226,67 @@
 
   .el-icon-arrow-down {
     font-size: 12px;
+  }
+
+  .box-card {
+    height: 40px;
+    display: flex;
+    align-items:center;;
+  }
+
+
+
+  .tags-li {
+    float: left;
+    margin: 0px 5px 0px 3px;
+    border-radius: 3px;
+    font-size: 12px;
+    overflow: hidden;
+    cursor: pointer;
+    height: 23px;
+    line-height: 23px;
+    border: 1px solid #e9eaec;
+    background: #fff;
+    padding: 0 5px 0 12px;
+    vertical-align: middle;
+    color: #666;
+    -webkit-transition: all .3s ease-in;
+    -moz-transition: all .3s ease-in;
+    transition: all .3s ease-in;
+  }
+
+  .tags-li:not(.active):hover {
+    background: #f8f8f8;
+  }
+
+  .tags-li-title {
+    float: left;
+    max-width: 80px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    margin-right: 5px;
+    color: #666;
+  }
+
+  .tags-li.active {
+    color: #fff;
+    border: 1px solid #10B9D3;
+    background-color: #10B9D3;
+  }
+
+  .tags-li.active .tags-li-title {
+    color: #fff;
+  }
+
+  .tags-close-box {
+    position: fixed;
+    right: 10px;
+    top: 50px;
+    box-sizing: border-box;
+    padding-top: 18px;
+    text-align: center;
+    z-index: 10;
+
   }
 </style>
